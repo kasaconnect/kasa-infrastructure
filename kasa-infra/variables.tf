064924405d0a613de4c802cf3fb646e735c8cf88
kasa-github <prashanth.s@vuedata.in>
2025-02-13 22:37:40 +0530

    removed aws_region in variables

--- kasa-infra/variables.tf
+++ kasa-infra/variables.tf
@@ -1,8 +1,3 @@
-variable "aws_region" {
-  description = "The AWS region to create resources in"
-  type        = string
-}
-
 variable "ami" {
   description = "The AMI ID for the instance"
   type        = string
