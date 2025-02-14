3029b116b2acc17169d072b099189ff48f111f13
Sachin Chandran <sachin.c@vuedata.in>
2025-02-14 13:14:36 +0530

    dynamodb updated

--- kasa-infra/backend.tf
+++ kasa-infra/backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     region               = "us-east-1"
     bucket               = "kasa-terraform-state-buckets"
-    dynamodb_table       = "kasa-terraform-state-lock"
+    dynamodb_table       = "demotf-locking"
     role_arn             = "arn:aws:iam::471112800881:role/TerraformAdmin"
     workspace_key_prefix = "environment"
     use_path_style       = true
