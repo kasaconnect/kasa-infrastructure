d54f32e4fbc23ebdb143a934f239b8a735edd500
kasa-github <prashanth.s@vuedata.in>
2025-02-14 09:14:08 +0530

    fix creds issue

--- kasa-infra/provider.tf
+++ kasa-infra/provider.tf
@@ -1,17 +1,17 @@
-provider "aws" {
-  region = var.region
+# provider "aws" {
+#   region = var.region
 
-  assume_role {
-    role_arn = "arn:aws:iam::${var.account_id}:role/TerraformAdmin"
-  }
+#   assume_role {
+#     role_arn = "arn:aws:iam::${var.account_id}:role/TerraformAdmin"
+#   }
 
-  default_tags {
-    tags = {
-      managed_by_terraform = true
-      repository           = "https://github.com/kasaconnect/kasa-infrastructure"
-    }
-  }
-}
+#   default_tags {
+#     tags = {
+#       managed_by_terraform = true
+#       repository           = "https://github.com/kasaconnect/kasa-infrastructure"
+#     }
+#   }
+# }
 
 provider "aws" {
   region = var.region
