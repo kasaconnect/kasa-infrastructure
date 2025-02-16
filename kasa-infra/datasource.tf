e2004b0a8b9531f09f5076a88863fc76aee92d9a
Sachin Chandran <sachin.c@vuedata.in>
2025-02-16 17:39:09 +0530

    added arameters

--- kasa-infra/datasource.tf
+++ kasa-infra/datasource.tf
@@ -1,19 +1,19 @@
------------------------------------------------------------------------------------------------------------
-Datasources
------------------------------------------------------------------------------------------------------------
+# -----------------------------------------------------------------------------------------------------------
+# Datasources
+# -----------------------------------------------------------------------------------------------------------
 
-Retrieve the account alias and company name stored by AFT - Use when neeeded
+# Retrieve the account alias and company name stored by AFT - Use when neeeded
 data "aws_ssm_parameter" "main" {
   for_each = toset(["account_alias", "company_name", "company_domain"])
 
   name = "/aft/account-request/custom-fields/${each.key}"
 }
 
-Retrieve the current AWS account ID - Use if neeeded
+# Retrieve the current AWS account ID - Use if neeeded
 data "aws_caller_identity" "current" {}
 
-Retrieve the current AWS region - Use if neeeded
+# Retrieve the current AWS region - Use if neeeded
 data "aws_region" "current" {}
 
-Retrieve the Availability Zones for the current region - Use if neeeded
+# Retrieve the Availability Zones for the current region - Use if neeeded
 data "aws_availability_zones" "available" {}
