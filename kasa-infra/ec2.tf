734284af50a7ed7a5df3edb3a29eeb7ec4e97ea8
Sachin Chandran <sachin.c@vuedata.in>
2025-02-16 18:32:09 +0530

    added Vpc and ec2

--- kasa-infra/ec2.tf
+++ kasa-infra/ec2.tf
@@ -1,10 +1,10 @@
 
-#referencing the resources in modules
+# #referencing the resources in modules
 
-module "ec2_instance" {
-  source        = "../modules/ec2"
-  ami           = var.ami
-  instance_type = var.instance_type
-}
+# module "ec2_instance" {
+#   source        = "../modules/ec2"
+#   ami           = var.ami
+#   instance_type = var.instance_type
+# }
 
-#add modules of resources as per requirement
+# #add modules of resources as per requirement
