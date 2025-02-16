dc084d4b23d8f48bf4e9b34a1961545cdeee5cfa
Sachin Chandran <sachin.c@vuedata.in>
2025-02-16 18:39:28 +0530

    added Vpc and ec2

--- kasa-infra/vpc.tf
+++ kasa-infra/vpc.tf
@@ -2,23 +2,23 @@
 # VPC configuration
 # -----------------------------------------------------------------------------------------------------------
 
-module "vpc" {
-  source  = "terraform-aws-modules/vpc/aws"
-  version = "5.12.0"
+# module "vpc" {
+#   source  = "terraform-aws-modules/vpc/aws"
+#   version = "5.12.0"
 
-  # Names and CIDR ranges
-  name = "${data.aws_ssm_parameter.main["account_alias"].value}-vpc"
-  cidr = var.vpc.vpc_cidr
+#   # Names and CIDR ranges
+#   name = "${data.aws_ssm_parameter.main["account_alias"].value}-vpc"
+#   cidr = var.vpc.vpc_cidr
 
-  # Availability Zones
-  azs = slice(data.aws_availability_zones.available.names, 0, 3)
+#   # Availability Zones
+#   azs = slice(data.aws_availability_zones.available.names, 0, 3)
 
-  # Subnets
-  public_subnets   = var.vpc.public_subnets
-  private_subnets  = var.vpc.private_subnets
-#   database_subnets = var.vpc.database_subnets
+#   # Subnets
+#   public_subnets   = var.vpc.public_subnets
+#   private_subnets  = var.vpc.private_subnets
+# #   database_subnets = var.vpc.database_subnets
 
-  # Additional settings
-  enable_nat_gateway = false
-  single_nat_gateway = false
-}
+#   # Additional settings
+#   enable_nat_gateway = false
+#   single_nat_gateway = false
+# }
