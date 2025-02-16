24dd0fb1579cb1ebf0d005d2f76419fa9bd4d1a9
Sachin Chandran <sachin.c@vuedata.in>
2025-02-16 17:37:20 +0530

    added arameter

--- kasa-infra/ssm.tf
+++ kasa-infra/ssm.tf
@@ -1,16 +1,18 @@
-data "aws_ssm_parameter" "main" {
-  name = "/app-runner/account_alias"
-}
 # -----------------------------------------------------------------------------------------------------------
 # SSM Parameters
 # -----------------------------------------------------------------------------------------------------------
+
+# Declare the missing data source for AWS SSM Parameter Store
+data "aws_ssm_parameter" "main" {
+  name = "/account-alias" # Ensure this is the correct parameter path in AWS SSM
+}
 
 module "ssm" {
   source   = "../modules/ssm"
   for_each = var.ssm
 
-  name        = "/app-runner/${data.aws_ssm_parameter.main["account_alias"].value}/${each.key}"
-  description = "App Runner parameters for ${data.aws_ssm_parameter.main["account_alias"].value}"
+  name        = "/app-runner/${data.aws_ssm_parameter.main.value}/${each.key}"
+  description = "App Runner parameters for ${data.aws_ssm_parameter.main.value}"
   type        = each.value == "{PLACEHOLDER}" ? "SecureString" : "String"
   value       = each.value
 }
